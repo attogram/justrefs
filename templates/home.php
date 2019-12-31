496e40d00a4425c9fb61394ad1c40e1455077cde
Attogram Project <attogram@users.noreply.github.com>
2019-12-31 11:36:26 +0100

    home

---
+++ templates/home.php
@@ -0,0 +1,17 @@
+<?php
+
+$menus = [
+    'Art', 'Culture', 'Entertainment', 'Geography', 'Health', 'History', 'Logic',
+    'Mathematics', 'Medicine', 'Nature', 'People', 'Philosophy', 'Religion',
+    'Society', 'Sport', 'Technology',
+];
+
+foreach ($menus as $menu) {
+    print '<a href="r/' . $menu . '">' . $menu . '</a> &nbsp; &nbsp;';
+}
+
+print '<br /><br />'
+    . '<form>'
+    . '<input name="q" value="" type="text" size="30">'
+    . '<input type="submit" value="   search  ">'
+    . '</form>';
