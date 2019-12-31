3995c6b7e415c53ceed1cb25014849071dd2e936
Attogram Project <attogram@users.noreply.github.com>
2019-12-31 11:36:19 +0100

    about

---
+++ templates/about.php
@@ -0,0 +1,31 @@
+<h1>Welcome to <b><?= $this->config['name'] ?></b></h1>
+<p>
+    The goal of this
+    <a href="../r/Website">website</a>
+    is to help 
+    <a href="../r/Student">students</a> and 
+    <a href="../r/Research">researchers</a>
+    by offering a <b>list of reference 
+    <a href="../r/Hyperlink">links</a></b>
+    and a <b>list of 
+    <a href="../r/Relation_(history_of_concept)">related</a> topics</b> 
+    for any 
+    <a href="../r/Index_term">topic</a> on the
+    <a href="../r/English_Wikipedia">English Wikipedia</a>.
+<p>
+<p>
+    This removes the
+    <a href="../r/Distraction">distraction</a> of the 
+    <a href="../r/Prose">prose</a> written by others,
+    and allows you to concentrate exclusively on
+    <a href="../r/Judgement">judging</a>
+    the quality the
+    <a href="../r/Reference">references</a>.
+</p>
+<p>
+    <b><a href="../"><?= $this->config['name'] ?></a></b> is not 
+    <a href="../r/Affiliate_(commerce)">affiliated</a>
+    with the
+    <a href="../r/Wikimedia_Foundation">Wikimedia Foundation</a>,
+    or any <a href="../r/List_of_Wikipedias">Wikipedia</a> website.
+</p>
