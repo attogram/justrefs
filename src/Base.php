f7b90022ee5ee08347ae276d8abe289548fba166
Attogram Project <attogram@users.noreply.github.com>
2020-04-19 13:04:04 +0200

    consts

--- src/Base.php
+++ src/Base.php
@@ -21,7 +21,7 @@ use function urldecode;
 
 class Base
 {
-    const VERSION = '0.5.0';
+    const VERSION = '0.5.1';
 
     /** Cache Time, in seconds.  4 days = 345600 seconds */
     const CACHE_TIME = 345600;
